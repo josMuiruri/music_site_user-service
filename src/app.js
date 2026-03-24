142c62b3c26c22b3650a3dde05fbdf6507ac2111
josMuiruri <josemuiruri51@gmail.com>
2026-03-24 23:04:57 +0300

    create server.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-const port = 3000;
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'))
+}
+
+app.use(express.json());
 
 app.use('/api/v1/users', userRouter);
 
-app.listen(port, () => {
-	console.log(`App running on port ${port}...`)
-});
 
 module.exports = app;
